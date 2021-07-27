0556638de5481de719c1526ad52522d04df69b8d
Irina Rusinova <rusinovarusya@mail.ru>
2021-07-27 21:26:21 +0300

    feat: do 7/8 of task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let arr = [];
+
+    let i = 0;
+
+    for (let key of matrix) {
+        for (let j = 0; j < key.length; ++j) {
+            if (i % 2 == 0) arr.push(key[j]);
+            else arr.push(key[key.length - 1 - j]);
+        } ++i;
+    }
+    return arr;
 }
